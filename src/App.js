c87471072bcb8f27d1b033b7cf2d4523e11c94e0
Leonardo <leonardo.venoso@live.com>
2022-10-02 15:16:50 +0800

    Added layout to app component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+import { Grid } from '@mui/material';
+import CustomBox from './components/CustomBox';
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example with styled-components
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Grid container spacing={5}>
+      <Grid item xs={12} md={8}>
+        <CustomBox>
+          <p>Time</p>
+        </CustomBox>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <CustomBox>
+          <p>Locations</p>
+        </CustomBox>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <CustomBox>
+          <p>Weather</p>
+        </CustomBox>
+      </Grid>
+    </Grid>
   );
-}
+};
