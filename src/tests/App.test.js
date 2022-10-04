98ffa2d980743f0af6d02f912e887d248df556eb
Leonardo <leonardo.venoso@live.com>
2022-10-04 14:58:53 +0800

    Test clean up, added App page object

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,65 +1,39 @@
-import { Provider } from 'react-redux';
 import store from '../rematch/store';
-import { render, fireEvent, within, waitFor } from '@testing-library/react/pure';
-import { LocalizationProvider } from '@mui/x-date-pickers';
+import { fireEvent, within, waitFor } from '@testing-library/react/pure';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import App from '../App';
 import * as apiTraffic from '../api/apiTraffic';
 import { trafficItems } from './trafficData';
+import AppPO from './pageObjects/AppPO';
 
 describe('App', () => {
   describe('when change time', () => {
-    let container;
-    let locationsInput;
+    let appPO;
 
     beforeAll(async () => {
       jest.spyOn(apiTraffic, 'fetchTraffic').mockResolvedValue(trafficItems);
-      container = render(
-        <Provider store={store}>
-          <LocalizationProvider dateAdapter={AdapterMoment}>
-            <App />
-          </LocalizationProvider>
-        </Provider>
-      );
-
-      const input = container.getByRole('textbox');
-      fireEvent.change(input, {target: {value: '2022-10-01 02:00'}});
-      fireEvent.keyDown(input, { key: 'Enter' });
-
-      const autocomplete = container.getByTestId('locations-autocomplete');
-      locationsInput = within(autocomplete).getByRole('combobox');
-      await waitFor(() => expect(locationsInput).toBeEnabled(), {timeout: 5000});
-
-      autocomplete.focus();
-      fireEvent.change(locationsInput, {target: {value: 'Kallang'}});
-      fireEvent.keyDown(autocomplete, {key: 'ArrowDown'});
-      fireEvent.keyDown(autocomplete, {key: 'Enter'});
+      appPO = new AppPO({store, dateAdapter: AdapterMoment});
+      appPO.renderApp();
+      appPO.selectTime('2022-10-01 02:00');
+      await appPO.selectLocation('Kallang');
     });
 
     afterAll(() => jest.resetAllMocks());
 
     it('filled location list', async () => {
-      expect(locationsInput.value).toContain('Kallang, (1.29531332, 103.871146)'); 
+      expect(appPO.getLocationsInputValue()).toContain('Kallang, (1.29531332, 103.871146)'); 
     });
 
     it('shows the location info', () => {
-      expect(container.getByTestId('weather')).toBeInTheDocument();
-      expect(container.getByTestId('coordinates')).toBeInTheDocument();
-      expect(container.getByTestId('cameraId')).toBeInTheDocument();
-      expect(container.getByTestId('locationName')).toBeInTheDocument();
+      expect(appPO.getByTestId('weather')).toBeInTheDocument();
+      expect(appPO.getByTestId('coordinates')).toBeInTheDocument();
+      expect(appPO.getByTestId('cameraId')).toBeInTheDocument();
+      expect(appPO.getByTestId('locationName')).toBeInTheDocument();
     });
 
     it('opens the image modal', () => {
-      container.getByTestId('locationImage').click();
+      appPO.openModal();
       waitFor(() => {
-        expect(container.getByTestId('imageModal')).toBeInTheDocument();
-      });
-    });
-
-    it('closes the modal', () => {
-      container.getByTestId('locationImage').click();
-      waitFor(() => {
-        expect(container.getByTestId('imageModal')).toBeInTheDocument();
+        expect(appPO.getByTestId('imageModal')).toBeInTheDocument();
       });
     });
   });
---
+++ src/tests/pageObjects/AppPO.js
@@ -0,0 +1,53 @@
+import { Provider } from 'react-redux';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { render, fireEvent, within, waitFor } from '@testing-library/react/pure';
+import App from '../../App';
+
+class AppPO  {
+  constructor({store, dateAdapter}) {
+    this.store = store;
+    this.dateAdapter = dateAdapter;
+    this.container;
+    this.locationsInput;
+  }
+
+  renderApp() {
+    this.container = render(
+      <Provider store={this.store}>
+        <LocalizationProvider dateAdapter={this.dateAdapter}>
+          <App />
+        </LocalizationProvider>
+      </Provider>
+  )};
+  
+  selectTime(time) {
+    const input = this.container.getByRole('textbox');
+    fireEvent.change(input, {target: {value: time}});
+    fireEvent.keyDown(input, { key: 'Enter' });
+  };
+
+  async selectLocation(locationName){
+    const autocomplete = this.container.getByTestId('locations-autocomplete');
+    this.locationsInput = within(autocomplete).getByRole('combobox');
+    await waitFor(() => expect(this.locationsInput).toBeEnabled(), {timeout: 5000});
+  
+    autocomplete.focus();
+    fireEvent.change(this.locationsInput, {target: {value: locationName}});
+    fireEvent.keyDown(autocomplete, {key: 'ArrowDown'});
+    fireEvent.keyDown(autocomplete, {key: 'Enter'});
+  };
+
+  openModal() {
+    this.container.getByTestId('locationImage').click();
+  };
+
+  getByTestId(id) {
+    return this.container.getByTestId(id);
+  };
+
+  getLocationsInputValue() {
+    return this.locationsInput.value;
+  };
+};
+
+export default AppPO;
