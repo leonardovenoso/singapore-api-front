092ace3db47ee1c9fee9004aa266e762abcb30ea
Leonardo <leonardo.venoso@live.com>
2022-10-05 13:56:00 +0800

    Removed unnecessary default return value when api call fails

--- src/api/apiTraffic.js
+++ src/api/apiTraffic.js
@@ -1,15 +1,7 @@
 const TRAFFIC_API = 'https://api.data.gov.sg/v1/transport/traffic-images';
 
 export const fetchTraffic = async (time) => {
-  try {
-    const resp = await fetch(`${TRAFFIC_API}?date_time=${time}`);
-    const json = await resp.json();
-    return json;
-  } catch (e) {
-    return {
-      items: {
-        cameras: [],
-      },
-    };
-  }
+  const resp = await fetch(`${TRAFFIC_API}?date_time=${time}`);
+  const json = await resp.json();
+  return json;
 };
